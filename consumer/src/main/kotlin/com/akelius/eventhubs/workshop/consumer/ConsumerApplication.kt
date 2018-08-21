92d851db79d7b04b2d0068528bc3fadc202c2684
Roman Tuchin <roman.tuchin@kreuzwerker.de>
2018-08-21 14:58:58 +0200

    add consumer

--- consumer/src/main/kotlin/com/akelius/eventhubs/workshop/consumer/ConsumerApplication.kt
+++ consumer/src/main/kotlin/com/akelius/eventhubs/workshop/consumer/ConsumerApplication.kt
@@ -3,16 +3,29 @@ package com.akelius.eventhubs.workshop.consumer
 import mu.KLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.stereotype.Component
+import java.util.*
 
 @SpringBootApplication
-@EnableScheduling
 class ConsumerApplication
 
 fun main(args: Array<String>) {
     runApplication<ConsumerApplication>(*args)
 }
 
+@Component
 class NewInvestmentSink {
     companion object: KLogging()
+
+    @KafkaListener(topics = ["new-investments"])
+    fun newInvestment(newInvestmentEvent: String) {
+        logger.info { "There is a new investment: $newInvestmentEvent" }
+    }
 }
+
+data class NewInvestmentEvent(
+        val eventId: UUID,
+        val type: String
+)
