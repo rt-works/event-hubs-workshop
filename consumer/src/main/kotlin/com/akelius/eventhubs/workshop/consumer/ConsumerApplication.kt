66000fa5c190add0cb7c3847db5d4fe9ba52680b
Roman Tuchin <roman.tuchin@kreuzwerker.de>
2018-08-21 14:05:53 +0200

    add basic components for producer and consumer

--- consumer/src/main/kotlin/com/akelius/eventhubs/workshop/consumer/ConsumerApplication.kt
+++ consumer/src/main/kotlin/com/akelius/eventhubs/workshop/consumer/ConsumerApplication.kt
@@ -1,11 +1,18 @@
 package com.akelius.eventhubs.workshop.consumer
 
+import mu.KLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class ConsumerApplication
 
 fun main(args: Array<String>) {
     runApplication<ConsumerApplication>(*args)
+}
+
+class NewInvestmentSink {
+    companion object: KLogging()
 }
--- producer/src/main/kotlin/com/akelius/eventhubs/workshop/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/com/akelius/eventhubs/workshop/producer/ProducerApplication.kt
@@ -1,11 +1,26 @@
 package com.akelius.eventhubs.workshop.producer
 
+import mu.KLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
+@EnableScheduling
 class ProducerApplication
 
 fun main(args: Array<String>) {
     runApplication<ProducerApplication>(*args)
+}
+
+@Component
+class NewInvestmentSource {
+    companion object : KLogging()
+
+    @Scheduled(fixedDelay = 30000)
+    fun sendNewInvestmentEvent() {
+        logger.info { "Sending new investment event" }
+    }
 }
