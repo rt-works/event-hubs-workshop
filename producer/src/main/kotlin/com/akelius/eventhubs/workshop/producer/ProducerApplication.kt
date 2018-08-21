5b3070d04959af38d3d3a839a1366dc6200a7d75
Roman Tuchin <roman.tuchin@kreuzwerker.de>
2018-08-21 14:36:18 +0200

    configure producer

--- producer/src/main/kotlin/com/akelius/eventhubs/workshop/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/com/akelius/eventhubs/workshop/producer/ProducerApplication.kt
@@ -1,11 +1,14 @@
 package com.akelius.eventhubs.workshop.producer
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import mu.KLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.util.*
 
 @SpringBootApplication
 @EnableScheduling
@@ -16,11 +19,27 @@ fun main(args: Array<String>) {
 }
 
 @Component
-class NewInvestmentSource {
+class NewInvestmentSource(
+        private val kafkaTemplate: KafkaTemplate<String, String>,
+        private val objectMapper: ObjectMapper
+) {
     companion object : KLogging()
 
     @Scheduled(fixedDelay = 30000)
     fun sendNewInvestmentEvent() {
-        logger.info { "Sending new investment event" }
+        logger.info { "Sending new investment event..." }
+
+        val newInvestmentEvent =
+                NewInvestmentEvent(UUID.randomUUID(), "ELECTRICAL")
+        val sendResult =
+                kafkaTemplate.sendDefault(objectMapper.writeValueAsString(newInvestmentEvent))
+
+        sendResult.addCallback({ result -> logger.info { "success" } },
+                { ex -> logger.info { "failure" } })
     }
 }
+
+data class NewInvestmentEvent(
+        val eventId: UUID,
+        val type: String
+)
